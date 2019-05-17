9c163ee8a8894800d2816c0e445f40645cf217d6
Sławomir Olszewski <slawomir.olszew@gmail.com>
2019-05-17 23:01:34 +0200

    added restart and closing messageBox

--- src/flappybird/FlappyBird.java
+++ src/flappybird/FlappyBird.java
@@ -5,9 +5,11 @@
  */
 package flappybird;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
@@ -15,6 +17,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -32,20 +35,41 @@ public class FlappyBird extends JFrame {
     private int positionY = 225;
     private final Timer timer;
     private boolean     controls[];
-    private int gravity = 1;
-    private int velocity = 0;
     private boolean released = true;
     
     class Controls extends TimerTask{
         
+        private int gravity = 1;
+        private int velocity = 0;
+        
         public void run(){
             if(controls[0]){
-                positionY -=45;
+                positionY += -velocity*10 ;
                 velocity = 0;
+                controls[0] = false;
+            }
+            else{
+                velocity +=gravity;
+                positionY += velocity;
             }
             
-            velocity +=gravity;
-            positionY += velocity;
+            if(positionY >= 483){
+                gravity = 0;
+                velocity = 0;
+                /*GAMEOVER*/
+                int answer = JOptionPane.showConfirmDialog(null, "Game Over, do you want to play again?", "GAME OVER", JOptionPane.YES_NO_OPTION);
+                System.out.println(answer);
+                if(answer == -1 | answer == 1){
+                    System.exit(0);
+                }
+                else
+                {
+                    positionY = 225;
+                    dispose();
+                    FlappyBird game = new FlappyBird();
+                    game.repaint();
+                }
+            }
             
             positionY = (positionY < 0)?0:positionY;
             repaint();
@@ -54,9 +78,12 @@ public class FlappyBird extends JFrame {
     
     public FlappyBird(){
         super("FlappyBird");
-        setBounds(50,50,1024,573);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int)(screenSize.getWidth() - 1024)/2;
+        int height = (int)(screenSize.getHeight()- 573)/2;
+        setBounds(width,height,1024,573);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
+        setResizable(false); 
         setVisible(true);
         createBufferStrategy(2);
         
@@ -76,14 +103,16 @@ public class FlappyBird extends JFrame {
                                 controls[0] = true;
                                 released = false;
                             }
+                            else{
+                                controls[0] = false;
+                            }
                             break;
                     }
             }
             public void keyReleased(KeyEvent e){
                 switch(e.getKeyCode()){
                     case KeyEvent.VK_SPACE:
-                        controls[0] = false;
-                        released = true;
+                        released = !released;
                         break;
                 }
             }
